d8d4597f9b9a52e0ac091ba7c3c90b8c9a30b27d
Rudra Gujarathi <rudra.gujarathi123@gmail.com>
2023-11-05 14:25:38 -0800

    requirements.txt added

--- QueryParser.py
+++ QueryParser.py
@@ -2,5 +2,9 @@ import re
 
 class QueryParser:
 
-    def __init__(self) -> None:
+    def __init__(self, query) -> None:
+        # get [age, name] from student join athlele on student.id = athlete.id where (cond) groupby col having (cond1) sort [cols] asc/desc.
+        self.query = query
+
+    def run_query(self):
         pass
