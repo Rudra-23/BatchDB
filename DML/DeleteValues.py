38b3c57f2fcf2e0696b67a55464e2e33f2596054
Rudra Gujarathi <rudra.gujarathi123@gmail.com>
2023-11-20 14:23:02 -0800

    delete values fixed

--- DML/DeleteValues.py
+++ DML/DeleteValues.py
@@ -31,36 +31,38 @@ class DeleteValues:
         batch_size = 100  
         start_row = 0
         end_row = batch_size
+        try:
+            while True:
+                df = pd.read_csv(file_name, skiprows=range(1, start_row), nrows=batch_size)
 
-        while True:
-            df = pd.read_csv(file_name, skiprows=range(1, start_row), nrows=batch_size)
+                if df.empty:
+                    break
 
-            if df.empty:
-                break
+                rows_to_delete = []
 
-            rows_to_delete = []
-
-            for index, item in df.iterrows():
-                namespace = {}
-                namespace.update(item)
-                try:
-                    if eval(self.where_conditions, namespace):
-                        rows_to_delete.append(index)
-                except:
-                    return "Incorrect syntax or variables."
+                for index, item in df.iterrows():
+                    namespace = {}
+                    namespace.update(item)
+                    try:
+                        if eval(self.where_conditions, namespace):
+                            rows_to_delete.append(index)
+                    except:
+                        return "Incorrect syntax or variables."
+                    
+                df = df.drop(rows_to_delete)
                 
-            df = df.drop(rows_to_delete)
-            
-            with open(tmp_file, 'a', newline="") as f:
-                if not columns:
-                    df.to_csv(f, header=True, index=False)
-                else:
-                    columns = True
-                    df.to_csv(f, header=False, index=False)
+                with open(tmp_file, 'a', newline="") as f:
+                    if not columns:
+                        df.to_csv(f, header=True, index=False)
+                    else:
+                        columns = True
+                        df.to_csv(f, header=False, index=False)
 
-            start_row = end_row
-            end_row += batch_size
+                start_row = end_row
+                end_row += batch_size
 
-        os.replace(tmp_file, file_name)
+            os.replace(tmp_file, file_name)
 
-        return "Items Deleted."
+            return "Items Deleted."
+        except:
+            return "Table not found!"
