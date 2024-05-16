7ab368e8116fabd7e8b0e5c8eb2947b725efee82
Rudra Gujarathi <rudra.gujarathi123@gmail.com>
2024-05-15 23:25:23 -0700

    tests updated

--- tests/test_mem.py
+++ tests/test_mem.py
@@ -9,7 +9,7 @@ class Project:
         self.tmp_dir = "./TMP/"
         self.data_dir = "./Data/"
 
-    def process_table(self):
+    def test_load_entire_data(self):
         try:
             file_name =  self.data_dir + f"{self.table_name}.csv"
 
@@ -23,8 +23,58 @@ class Project:
         except:
             return "err"
         
-    def display_values(self):
-        status = self.process_table()
+    def test_load_in_chunks(self):
+        try:
+            file_name =  self.data_dir + f"{self.table_name}.csv"
+
+            batch_size = 100
+            start_row = 0
+            end_row = batch_size
+
+            df = pd.read_csv(file_name, nrows = batch_size, skiprows= range(1, start_row))
+            
+            header = True
+
+            self.cols = self.cols if self.cols != [''] else list(df.columns)
+
+            if df.empty:
+                table = tabulate([self.cols], tablefmt="fancy_grid", headers = "firstrow")
+                print(table)
+                return "success"
+
+            while df is not None and not df.empty:
+                data = []
+                
+                for _, item in df.iterrows():
+                    if len(self.cols) == 0:
+                        data.append(item.values)
+                    else:
+                        selected_data = [item[col] for col in self.cols]
+                        data.append(selected_data)
+
+                if data:
+                    if header == True:
+                        header = False
+                        table = tabulate(data, headers= list(self.cols), tablefmt="fancy_grid")
+                    else:
+                        table = tabulate(data, tablefmt="fancy_grid")
+                    print(table)
+
+                start_row = end_row + 1
+                end_row += batch_size
+                df = pd.read_csv(file_name, nrows = batch_size, skiprows= range(1, start_row))
+
+            return "success"
+        except:
+            return "err"
+        
+    def display_values(self, test: bool):
+        status = None
+
+        if test == True:
+            status = self.test_load_entire_data()
+        else:
+            status = self.test_load_in_chunks()
 
         if status == 'err':
             raise SyntaxError("Error: Some error occurred while projecting. Please check variables")
@@ -33,9 +83,13 @@ class Project:
 if __name__ == "__main__":
     
     while True:
-        table = input("Enter table name [large] or [exit]: ")
-        if table == 'exit':
+        request = input("Enter [LoadAll or LoadChunks] or [exit]: ")
+        if request == 'exit':
             break
         
-        obj = Project(table, [""])
-        obj.display_values()
+        obj = Project("large", [""])
+        
+        if request == "LoadAll":
+            obj.display_values(True)
+        else:
+            obj.display_values(False)
